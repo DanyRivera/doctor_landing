66c827b3a2e6da36b531d43e7699b04e8f2bd660
Dany <dany@gmail.com>
2025-09-18 19:40:01 -0600

    first commit

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <p>Ok</p>
         {children}
       </body>
     </html>

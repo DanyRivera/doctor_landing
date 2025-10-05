6780cbb4a48ae341a79786aaad5ce17c8349029d
Dany <dany@gmail.com>
2025-10-04 20:14:59 -0600

    Done: Animations

--- components/ui/Slider.jsx
+++ components/ui/Slider.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react"
 import Review from "./Review"
 import reviews from "@/app/data/reviews.json";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Slider = () => {
 
@@ -33,11 +34,21 @@ const Slider = () => {
                         <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
                     </svg>
                 </button>
-                <div className="m-10 flex flex-col md:flex-row md:justify-center gap-10 contenedor ">
-                    {visibleReviews.map((review, index) => (
-                        <Review key={index} name={review.name} location={review.location} text={review.text} image={review.image} />
-                    ))}
-                </div>
+                <AnimatePresence mode="wait">
+                    <motion.div
+                        key={currentIndex}
+                        initial={{ opacity: 0, x: 100 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: -100 }}
+                        transition={{ duration: 0.1 }}
+                        className="m-10 flex flex-col md:flex-row md:justify-center gap-10 contenedor"
+                    >
+                        {visibleReviews.map((review, index) => (
+                            <Review key={index} name={review.name} location={review.location} text={review.text} image={review.image} />
+                        ))}
+                    </motion.div>
+                </AnimatePresence>
+  
                 <button onClick={nextSlide}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-10 text-primary cursor-pointer">
                         <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />

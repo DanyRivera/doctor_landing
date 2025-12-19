5653f7524ca2dd3a4ad962f7860a785b30e60ca8
Dany <dany@gmail.com>
2025-12-18 18:44:16 -0600

    Add: Services Record

--- components/Services.jsx
+++ components/Services.jsx
@@ -13,25 +13,25 @@ const Services = () => {
                     <motion.h2 initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.8}} className="text-center font-bold text-2xl uppercase">Servicios</motion.h2>
                     <motion.p initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.8}} className="text-center mx-5 text-gray-500 md:w-1/2 md:mx-auto">En esta sección encontrarás los servicios médicos que el Dr. Daniel Rivera pone a tu disposición, diseñados para ofrecer atención integral y de calidad.</motion.p>
                     <div className="flex flex-col gap-10 mt-5 md:grid md:grid-cols-4 ">
-                        <motion.div initial={{opacity: 0, x: -200}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.8}}  className="bg-white mx-10 p-5 flex flex-col gap-3 rounded-xl shadow-xl md:mx-0">
+                        <motion.div initial={{opacity: 0, x: -200}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.8}} viewport={{once:true}}  className="bg-white mx-10 p-5 flex flex-col gap-3 rounded-xl shadow-xl md:mx-0">
                             <Image className="bg-secondary p-2 rounded-full" src="/images/heart.svg" width={70} height={0} alt="Circles" />
                             <p className="text-xl font-bold">Cardiología</p>
                             <p>Atención especializada en la prevención, diagnóstico y tratamiento de enfermedades del corazón y el sistema circulatorio.</p>
                         </motion.div>
 
-                        <motion.div initial={{opacity: 0, x: -100}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.8}} className="bg-white mx-10 p-5 flex flex-col gap-3 rounded-xl shadow-xl md:mx-0">
+                        <motion.div initial={{opacity: 0, x: -100}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.8}} viewport={{once:true}} className="bg-white mx-10 p-5 flex flex-col gap-3 rounded-xl shadow-xl md:mx-0">
                             <Image className="bg-secondary p-2 rounded-full" src="/images/brain.svg" width={70} height={0} alt="Circles" />
                             <p className="text-xl font-bold">Neurología</p>
                             <p>Diagnóstico y manejo de trastornos que afectan al sistema nervioso, como migrañas, epilepsia y Parkinson.</p>
                         </motion.div>
 
-                        <motion.div initial={{opacity: 0, x: 100}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.8}} className="bg-white mx-10 p-5 flex flex-col gap-3 rounded-xl shadow-xl md:mx-0">
+                        <motion.div initial={{opacity: 0, x: 100}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.8}} viewport={{once:true}} className="bg-white mx-10 p-5 flex flex-col gap-3 rounded-xl shadow-xl md:mx-0">
                             <Image className="bg-secondary p-2 rounded-full" src="/images/x-ray.svg" width={70} height={0} alt="Circles" />
                             <p className="text-xl font-bold">Radiología</p>
                             <p>Servicios de imagen médica con tecnología avanzada para un diagnóstico rápido y seguro.</p>
                         </motion.div>
 
-                        <motion.div initial={{opacity: 0, x: 200}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.8}} className="bg-white mx-10 p-5 flex flex-col gap-3 rounded-xl shadow-xl md:mx-0">
+                        <motion.div initial={{opacity: 0, x: 200}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.8}} viewport={{once:true}} className="bg-white mx-10 p-5 flex flex-col gap-3 rounded-xl shadow-xl md:mx-0">
                             <Image className="bg-secondary p-2 rounded-full" src="/images/person.svg" width={70} height={0} alt="Circles" />
                             <p className="text-xl font-bold">Urología</p>
                             <p>Atención especializada en la salud del sistema urinario y el aparato reproductor masculino.</p>
